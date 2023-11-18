953058ad63cc95dd41f96518ec95c50cf76a46cc
DESKTOP-S3T27CJ\takua <takuanoishiiyona@outlook.jp>
2023-11-18 19:22:54 +0900

    リストの順番とファイルが存在しない場合の例外処理を追加

--- database/request_to_database.py
+++ database/request_to_database.py
@@ -1,11 +1,13 @@
 import sqlite3
+import os
 
 # 最小緯度、最小経度、最大緯度、最大経度
 def getShelterList(left_down_latitude, left_down_longitude, right_up_latitude, right_up_longitude):
     dbname = 'shelter.db'
+    if not os.path.exists(dbname): return None
     con = sqlite3.connect(dbname)
     cur = con.cursor()
-    req = f"SELECT 施設・場所名, 緯度, 経度 FROM shelters WHERE 緯度 >= {left_down_latitude} AND 緯度 <= {right_up_latitude} AND 経度 >= {left_down_longitude} AND 経度 <= {right_up_longitude};"
+    req = f"SELECT 緯度, 経度, 施設・場所名 FROM shelters WHERE 緯度 >= {left_down_latitude} AND 緯度 <= {right_up_latitude} AND 経度 >= {left_down_longitude} AND 経度 <= {right_up_longitude};"
     cur.execute(req)
     return cur.fetchall()
 
